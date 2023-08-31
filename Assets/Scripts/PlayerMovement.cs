f0f5fd165c72394276432013e01bcb34a1db910d
Brad Ziegler <bradleyziegler72@gmail.com>
2023-08-30 21:58:42 -0400

    Added basic movement to the player based off of touchscreen input.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,21 +4,37 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour {
+    [SerializeField] private float forceMagnitude;
+    [SerializeField] private float maxVelocity;
+
+    private Rigidbody rb;
     private Camera mainCamera;
 
+    private Vector3 movementDirection;
+
     void Start() {
+        rb = GetComponent<Rigidbody>();
         mainCamera = Camera.main;
     }
 
     void Update() {
         if (Touchscreen.current.primaryTouch.press.isPressed) {
             Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
-
-            Debug.Log(touchPosition);
-
             Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
 
-            Debug.Log(worldPosition);
+            movementDirection = transform.position - worldPosition;
+            movementDirection.z = 0f;
+            movementDirection.Normalize();
+        } else {
+            movementDirection = Vector3.zero;
         }
+    }
+
+    void FixedUpdate() {
+        if (movementDirection == Vector3.zero) { return; }
+
+        rb.AddForce(movementDirection * forceMagnitude, ForceMode.Force);
+
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxVelocity);
     }
 }
